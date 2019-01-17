1afd9736ef44c0d07ad96f7474c0147a823a0e7c
Anna Takacs <annatakacs96@gmail.com>
2019-01-17 13:03:23 +0100

    add greet

---
+++ week-01/day-04/greet/cmake-build-debug/CMakeFiles/greet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greet.dir/main.cpp.obj"
+  "greet.pdb"
+  "greet.exe"
+  "greet.exe.manifest"
+  "libgreet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
