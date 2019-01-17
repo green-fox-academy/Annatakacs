bbefedc3ae95b1a8fc0b2daf6ad5453229a5d1d1
Anna Takacs <annatakacs96@gmail.com>
2019-01-18 00:57:07 +0100

    add change_element

---
+++ week-01/day-04/change_element/cmake-build-debug/CMakeFiles/change_element.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/change_element.dir/main.cpp.obj"
+  "change_element.pdb"
+  "change_element.exe"
+  "change_element.exe.manifest"
+  "libchange_element.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/change_element.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
