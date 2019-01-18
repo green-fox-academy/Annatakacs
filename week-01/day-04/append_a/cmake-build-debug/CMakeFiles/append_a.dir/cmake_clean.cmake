20d132653572b31909866738d89ef03cedbf6b58
Takács Anna <annatakacs96@gmail.com>
2019-01-18 15:10:12 +0100

    add append_a

---
+++ week-01/day-04/append_a/cmake-build-debug/CMakeFiles/append_a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/append_a.dir/main.cpp.o"
+  "append_a.pdb"
+  "append_a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/append_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
