247c2ad4acc9f42cd8413680fb66aa1bdcad753d
Takács Anna <annatakacs96@gmail.com>
2019-01-18 17:00:38 +0100

    add sum_all

---
+++ week-01/day-04/sum_all/cmake-build-debug/CMakeFiles/sum_all.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sum_all.dir/main.cpp.o"
+  "sum_all.pdb"
+  "sum_all"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sum_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
