68736db6982ed43df8a86cfd8f2569ff1a0dce32
Takács Anna <annatakacs96@gmail.com>
2019-01-18 15:22:09 +0100

    add swap_elements

---
+++ week-01/day-04/swap_elements/cmake-build-debug/CMakeFiles/swap_elements.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/swap_elements.dir/main.cpp.o"
+  "swap_elements.pdb"
+  "swap_elements"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/swap_elements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
