c6d8a543c5e66e16dff6d7b849572e5c1eae9501
Anna Takacs <annatakacs96@gmail.com>
2019-01-18 07:15:43 +0100

    add print_all

---
+++ week-01/day-04/print_all/cmake-build-debug/CMakeFiles/print_all.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/print_all.dir/main.cpp.obj"
+  "print_all.pdb"
+  "print_all.exe"
+  "print_all.exe.manifest"
+  "libprint_all.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/print_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
