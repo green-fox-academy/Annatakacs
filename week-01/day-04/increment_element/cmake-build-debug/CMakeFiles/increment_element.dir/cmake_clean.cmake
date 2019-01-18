de2167b8a11700d26446d1c27f867de9f7d5cb10
Anna Takacs <annatakacs96@gmail.com>
2019-01-18 01:27:08 +0100

    add increment_element

---
+++ week-01/day-04/increment_element/cmake-build-debug/CMakeFiles/increment_element.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/increment_element.dir/main.cpp.obj"
+  "increment_element.pdb"
+  "increment_element.exe"
+  "increment_element.exe.manifest"
+  "libincrement_element.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/increment_element.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
