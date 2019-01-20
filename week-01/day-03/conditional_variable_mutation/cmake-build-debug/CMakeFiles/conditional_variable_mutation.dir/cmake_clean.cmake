fa7f67ae89ab699bec8e7387c6be4ae9863091c4
Takács Anna <annatakacs96@gmail.com>
2019-01-20 21:04:24 +0100

    add cond_var_mut

---
+++ week-01/day-03/conditional_variable_mutation/cmake-build-debug/CMakeFiles/conditional_variable_mutation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/conditional_variable_mutation.dir/main.cpp.o"
+  "conditional_variable_mutation.pdb"
+  "conditional_variable_mutation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/conditional_variable_mutation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
