627a8ff456de6128d15b81e62b51149c9f877d20
Takács Anna <annatakacs96@gmail.com>
2019-01-20 14:59:11 +0100

    add odd_even

---
+++ week-01/day-03/odd_even/cmake-build-debug/CMakeFiles/odd_even.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/odd_even.dir/main.cpp.o"
+  "odd_even.pdb"
+  "odd_even"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odd_even.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
