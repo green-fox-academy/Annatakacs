870c611dbf5e767f431f6c52e4fff483d094f22f
Takács Anna <annatakacs96@gmail.com>
2019-04-04 11:22:40 +0200

    Change doupling exercise

--- week-01/day-03/draw_diamond/main.cpp
+++ week-01/day-03/draw_diamond/main.cpp
@@ -17,7 +17,25 @@ int main(int argc, char* args[]) {
     //
     // The diamond should have as many lines as the number was
 
+    int rows = 7;
 
+    //std::cout << "Please give me a number" << std::endl;
+   // std::cin >> rows;
 
+    for (int i = 1; i <= rows; i++) {
+        for (int spaces = i; spaces < rows; spaces++) {
+            std::cout << " ";
+        } for (int j = 1; j < (2 * (i - 1)); j++) {
+            std::cout << "*";
+        } std::cout << "" << std::endl;
+    }
+
+    /*for (int k = (rows / 2); k >= 0; k--) {
+        for (int spaces2 = 1; spaces2 < (rows/2); spaces2--) {
+            std::cout << " ";
+        } for (int l = (rows / 2); l >= 0; l--) {
+            std::cout << "*";
+        } std::cout << "" << std::endl;
+    } */
     return 0;
 }
---
+++ week-01/day-03/draw_square/cmake-build-debug/CMakeFiles/draw_square.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/draw_square.dir/main.cpp.o"
+  "draw_square.pdb"
+  "draw_square"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/draw_square.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
