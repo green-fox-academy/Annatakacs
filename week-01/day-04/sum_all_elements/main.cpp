e41ffaf249595032ba830efe7c2bac21e0651945
Takács Anna <annatakacs96@gmail.com>
2019-04-04 16:25:02 +0200

    sum_all_elements

--- week-01/day-04/sum_all_elements/main.cpp
+++ week-01/day-04/sum_all_elements/main.cpp
@@ -9,12 +9,17 @@ int main(int argc, char* args[]) {
     // - Print the sum of the elements in `ai`
 
     int ai[] = {3, 4, 5, 6, 7};
+    int size = sizeof(ai) / sizeof(ai[0]);
+
     int sum = 0;
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < size; i++) {
         sum += ai[i];
     }
+
     std::cout << sum << std::endl;
+
 
     return 0;
 }
+
