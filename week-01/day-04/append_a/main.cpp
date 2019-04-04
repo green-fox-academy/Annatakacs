ee8cfeba95ce496da9c181808933ff3e99227be3
Takács Anna <annatakacs96@gmail.com>
2019-04-04 15:20:09 +0200

    change append_a

--- week-01/day-04/append_a/main.cpp
+++ week-01/day-04/append_a/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+void appendFunc(std::string[], int);
+
 int main(int argc, char* args[]) {
 
     // - Create an array variable named `animals`
@@ -8,12 +10,17 @@ int main(int argc, char* args[]) {
     // - Add all elements an `"a"` at the end
 
     std::string animals[] = {"koal", "pand", "zebr"};
+    int size = sizeof(animals) / sizeof(animals[0]);
 
-    for (int i = 0; i < 3; i++){
-        animals[i] += "a";
-    }
-    for (int i = 0; i < 3; i++){
-        std::cout << animals[i] << std::endl;
-    }
+    appendFunc(animals, size);
+
     return 0;
+}
+void appendFunc(std::string x[], int y) {
+    for (int i = 0; i < y; i++) {
+        x[i] += 'a';
+    }
+    for (int j = 0; j < y; j++) {
+        std::cout << x[j] << std::endl;
+    }
 }
