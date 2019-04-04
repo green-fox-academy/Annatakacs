12ba0b598298165483c4079c5b2b3b07f9945286
Takács Anna <annatakacs96@gmail.com>
2019-04-04 15:10:58 +0200

    Complete colors

--- week-01/day-04/matrix/main.cpp
+++ week-01/day-04/matrix/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char* args[]) {
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-        std::cout << array[i][j] << " ";
+            std::cout << array[i][j] << " ";
         }
         std::cout << std::endl;
     }
