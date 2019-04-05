42017bba7215f661cc2a9aa2d4581eccfd0156e5
Takács Anna <annatakacs96@gmail.com>
2019-04-05 17:28:46 +0200

    Finished square

--- week-01/day-03/draw_square/main.cpp
+++ week-01/day-03/draw_square/main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-int main(int argc, char* args[]) {
+int main(int argc, char *args[]) {
 
     // Write a program that reads a number from the standard input, then draws a
     // square like this:
@@ -22,7 +22,22 @@ int main(int argc, char* args[]) {
     std::cout << "Please give me a number" << std::endl;
     std::cin >> rows;
 
+    for (int i = 0; i < rows; i++) {
+        if (i == 0 || i == (rows - 1)) {
+            for (int j = 0; j < rows; j++) {
+                std::cout << "%";
+            }
+            std::cout << std::endl;
+        } else {
+            for (int i = 0; i < rows; i++) {
+                if (i == 0 || i == (rows -1)) {
+                    std::cout << "%";
+                } else {
+                    std::cout << " ";
+                }
+            } std::cout << std::endl;
+        }
+    }
 
-    return 0;
-}
-}
+        return 0;
+    }
