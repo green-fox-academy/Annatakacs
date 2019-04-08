9d565e6c907822e367290995c3c0dba046ed025e
Takács Anna <annatakacs96@gmail.com>
2019-04-08 14:12:26 +0200

    Finished swap

--- week-02/day-01/swap/main.cpp
+++ week-02/day-01/swap/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void swap(int *x, int *y);
+
 int main ()
 {
     // Create a function which swaps the values of 'a' and 'b'
@@ -11,9 +13,19 @@ int main ()
     int *aPointer = &a;
     int *bPointer = &b;
 
+    std::cout << "The value of a before swap: " << *aPointer << std::endl;
+    std::cout << "The value of b before swap: " << *bPointer << std::endl;
+
+    swap(aPointer, bPointer);
+
+    std::cout << "The value of a after swap: " << *aPointer << std::endl;
+    std::cout << "The value of b after swap: " << *bPointer << std::endl;
+
 
     return 0;
 }
 void swap(int *x, int *y) {
-
+    int temp = *y;
+    *y = *x;
+    *x = temp;
 }
