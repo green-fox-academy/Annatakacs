245e4706451c8838ec099ce275dff53189717eb6
Takács Anna <annatakacs96@gmail.com>
2019-04-08 12:59:58 +0200

    Finished adding

---
+++ week-02/day-01/adding/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // Add two numbers using pointers
+
+    int a = 20;
+    int b = 17;
+
+    int *aPointer = &a;
+    int *bPointer = &b;
+
+    *aPointer += *bPointer;
+
+    std::cout << *aPointer;
+    
+    return 0;
+}
