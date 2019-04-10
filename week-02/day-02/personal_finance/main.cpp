1ce1702b830bc493c5c96f8edeb997f0d3a6f4a7
Takács Anna <annatakacs96@gmail.com>
2019-04-10 14:50:39 +0200

    Finished personal_finance

--- week-02/day-02/personal_finance/main.cpp
+++ week-02/day-02/personal_finance/main.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
 #include <vector>
 
+int sum(std::vector<int> x);
+
+int greatestExpense(std::vector<int> x);
+
+int cheapestExpense(std::vector<int> x);
+
+int average(std::vector<int> x, int y);
+
 int main() {
 
     std::vector<int> myExpenses = {500, 1000, 1250, 175, 800, 120};
 
-    //How much did we spend?
+    //std::vector<int>::iterator myExpensesIt = myExpenses.begin();
 
-    int sum;
-    for (unsigned int i = 0; i < myExpenses.size(); i++) {
-        sum += myExpenses.at(i);
-    }
-    std::cout << "We spent: " << sum << std::endl;
+    int all = sum(myExpenses);
+    std::cout << "We spent: " << all << std::endl;
 
-    //Which was our greatest expense?
+    std::cout << "Our greatest expense was: " << greatestExpense(myExpenses) << std::endl;
 
-    int max;
-    std::vector<int>::iterator myExpensesIt = myExpenses.begin();
-    for(; myExpensesIt != myExpenses.end(); myExpensesIt++) {
-        if (myExpensesIt > (myExpensesIt+1)) {}
-    }
+    std::cout << "Our cheapest spending was: " << cheapestExpense(myExpenses) << std::endl;
 
+    std::cout << "Our average spending was: " << average(myExpenses, all) << std::endl;
 
     return 0;
+}
+
+int sum(std::vector<int> x) {
+    int sum = 0;
+    for (unsigned int i = 0; i < x.size(); i++) {
+        sum = sum + x[i];
+    }
+    return sum;
+}
+
+int greatestExpense(std::vector<int> x) {
+    int max = 0;
+    for (unsigned int i = 0; i < x.size(); i++) {
+        if (max < x[i]) {
+            max = x[i];
+        }
+    }
+    return max;
+}
+
+int cheapestExpense(std::vector<int> x) {
+    int min = x.at(0);
+    for (unsigned int i = 0; i < x.size(); i++) {
+        if (min > x[i]) {
+            min = x[i];
+        }
+    }
+    return min;
+}
+
+int average(std::vector<int> x, int y) {
+    int size = x.size();
+    return (y/size);
 }
