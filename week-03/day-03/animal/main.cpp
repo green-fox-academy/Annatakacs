2b0b60f6e40cecaf2280726d2e8ce3819cf10dc6
Takács Anna <annatakacs96@gmail.com>
2019-04-17 15:19:34 +0200

    Finished animal

---
+++ week-03/day-03/animal/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Animal.h"
+
+int main()
+{
+    Animal dog;
+    dog.play();
+    std::cout << dog.gethunger() << ' ' << dog.getthirst() << std::endl;
+
+    Animal cat;
+    cat.eat();
+    std::cout << cat.gethunger() << ' ' << cat.getthirst() << std::endl;
+
+    Animal parrot;
+    parrot.drink();
+    std::cout << parrot.gethunger() << ' ' << parrot.getthirst() << std::endl;
+
+    return 0;
+}
