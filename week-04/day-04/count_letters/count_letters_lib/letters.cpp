e2a3045620c8ea99c39b7a6c8e8404aef0b20dd9
Takács Anna <annatakacs96@gmail.com>
2019-04-25 15:05:58 +0200

    Finished testing count_letters

--- week-04/day-04/count_letters/count_letters_lib/letters.cpp
+++ week-04/day-04/count_letters/count_letters_lib/letters.cpp
@@ -7,11 +7,8 @@
 std::map<std::string, int> Letters::makingDictionay(std::string word)
 {
     std::map<std::string, int> myMap;
-    //std::map<std::string, int>::iterator it;
-    std::string x;
     for (unsigned int i = 0; i < word.size(); ++i) {
-        x = word.substr(i, 1);
-        myMap.insert(std::make_pair(x, i+1));
+        myMap[word.substr(i, 1)]++;
     }
     return myMap;
 }
--- week-04/day-04/count_letters/count_letters_test/count_letters_test.cpp
+++ week-04/day-04/count_letters/count_letters_test/count_letters_test.cpp
@@ -12,8 +12,26 @@ TEST(TestDictionary, SeeIfMapIsReady)
 
     // Assert
     ASSERT_EQ(myMap["B"], 1);
-    ASSERT_EQ(myMap["E"], 2);
-    ASSERT_EQ(myMap["L"], 3);
-    ASSERT_EQ(myMap["A"], 4);
+    ASSERT_EQ(myMap["E"], 1);
+    ASSERT_EQ(myMap["L"], 1);
+    ASSERT_EQ(myMap["A"], 1);
 }
 
+TEST(TestDictionary, SeeIfMapIsReady2)
+{
+    //Arrange
+    Letters letters;
+    letters.word = "KONSTANTIN";
+
+    //Act
+    std::map<std::string, int> myMap = letters.makingDictionay(letters.word);
+
+    // Assert
+    ASSERT_EQ(myMap["K"], 1);
+    ASSERT_EQ(myMap["O"], 1);
+    ASSERT_EQ(myMap["N"], 3);
+    ASSERT_EQ(myMap["S"], 1);
+    ASSERT_EQ(myMap["T"], 2);
+    ASSERT_EQ(myMap["A"], 1);
+    ASSERT_EQ(myMap["I"], 1);
+}
