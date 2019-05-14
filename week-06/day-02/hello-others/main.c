ca43c5bb4164a41b97127469099bd902f4f5b888
Takács Anna <annatakacs96@gmail.com>
2019-05-14 11:41:52 +0200

    Finished greeting-others exercise

---
+++ week-06/day-02/hello-others/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    char name1[10] = "Balage";
+    char name2[10] = "Sanyi";
+    char name3[10] = "Esther";
+
+    printf("Hello, %s!\n", name1);
+    printf("Hello, %s!\n", name2);
+    printf("Hello, %s!\n", name3);
+
+    puts("Hello, Radars!");
+
+    return 0;
+}
