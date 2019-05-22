20480107f34708a19a70a3bc819cc847ccc38a0e
Takács Anna <annatakacs96@gmail.com>
2019-05-22 14:20:52 +0200

    Flashes 4 LED diodes

---
+++ week-07/day-03/flashing_multiple_led/main.c
@@ -0,0 +1,60 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+
+int main(void) {
+
+	HAL_Init();
+
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN; /* GPIOI clock enable */
+
+		// Init user led (PF10)
+		GPIOF->MODER |= (GPIO_MODER_MODER10_0); /* configure PI1 in output mode */
+		GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_10); /* ensure push pull mode selected default */
+		GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR10); /* ensure maximum speed setting (even though it is unnecessary) */
+		GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR10); /* ensure all pull up pull down resistors are disabled */
+
+		// Init user led (PF10)
+		GPIOF->MODER |= (GPIO_MODER_MODER9_0); /* configure PI1 in output mode */
+		GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_9); /* ensure push pull mode selected default */
+		GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR9); /* ensure maximum speed setting (even though it is unnecessary) */
+		GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR9); /* ensure all pull up pull down resistors are disabled */
+
+
+		// Init user led (PF10)
+		GPIOF->MODER |= (GPIO_MODER_MODER8_0); /* configure PI1 in output mode */
+		GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_8); /* ensure push pull mode selected default */
+		GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR8); /* ensure maximum speed setting (even though it is unnecessary) */
+		GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR8); /* ensure all pull up pull down resistors are disabled */
+
+
+		// Init user led (PF10)
+		GPIOF->MODER |= (GPIO_MODER_MODER7_0); /* configure PI1 in output mode */
+		GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_7); /* ensure push pull mode selected default */
+		GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR7); /* ensure maximum speed setting (even though it is unnecessary) */
+		GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR7); /* ensure all pull up pull down resistors are disabled */
+
+
+	uint32_t red_led = ((1 << 10) | (1 << 9) | (1 << 8) | (1 << 7));
+
+	while (1) {
+
+		GPIOF->BSRR = red_led;
+		HAL_Delay(500);
+		GPIOF->BSRR = red_led << 16;
+		HAL_Delay(500);
+
+
+	}
+}
