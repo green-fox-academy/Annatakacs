975ebe24c83a28ba56345d33debf026e1c2f892c
Takács Anna <annatakacs96@gmail.com>
2019-05-22 17:27:23 +0200

    LED flashes in the pattern of knight rider and changes direction when the button is pushed

---
+++ week-07/day-03/change_direction_knight_rider/main.c
@@ -0,0 +1,80 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+int main(void) {
+
+	HAL_Init();
+
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN; /* GPIOI clock enable */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+	// Init user led (PF10)
+	GPIOF->MODER |= (GPIO_MODER_MODER10_0); /* configure PI1 in output mode */
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_10); /* ensure push pull mode selected default */
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR10); /* ensure maximum speed setting (even though it is unnecessary) */
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR10); /* ensure all pull up pull down resistors are disabled */
+
+	// Init user led (PF10)
+	GPIOF->MODER |= (GPIO_MODER_MODER9_0); /* configure PI1 in output mode */
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_9); /* ensure push pull mode selected default */
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR9); /* ensure maximum speed setting (even though it is unnecessary) */
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR9); /* ensure all pull up pull down resistors are disabled */
+
+
+	// Init user led (PF10)
+	GPIOF->MODER |= (GPIO_MODER_MODER8_0); /* configure PI1 in output mode */
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_8); /* ensure push pull mode selected default */
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR8); /* ensure maximum speed setting (even though it is unnecessary) */
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR8); /* ensure all pull up pull down resistors are disabled */
+
+
+	// Init user led (PF10)
+	GPIOF->MODER |= (GPIO_MODER_MODER7_0); /* configure PI1 in output mode */
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_7); /* ensure push pull mode selected default */
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR7); /* ensure maximum speed setting (even though it is unnecessary) */
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR7); /* ensure all pull up pull down resistors are disabled */
+
+	GPIOB->MODER |= (1 << 4);
+
+	uint32_t red_led = (1 << 6);
+
+	uint32_t user_button = (1 << 4);
+
+	int direction = -1;
+
+	while (1) {
+		if (direction == -1) {
+			red_led <<= 1;
+			GPIOF -> BSRR = red_led;
+			HAL_Delay(200);
+			GPIOF -> BSRR = red_led << 16;
+			if (red_led == (1 << 11)) {
+				red_led = (1 << 6);
+			}
+		} else {
+			red_led >>= 1;
+			GPIOF -> BSRR = red_led;
+			HAL_Delay(200);
+			GPIOF -> BSRR = red_led << 16;
+			if (red_led == (1 << 6)) {
+				red_led = (1 << 11);
+			}
+		}
+		if (GPIOB->IDR & (user_button)) {
+			direction *= -1;
+		}
+	}
+}
+
+
